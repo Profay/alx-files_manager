f5b5b502e8565a132621e8a9fba6d1da55c2b233
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-04 16:24:00 +0000

    usercontroller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const route = express.Router();
 
 route.get('/status', AppController.getStatus);
 route.get('/stats', AppController.getStats);
+route.get('/users', UsersController.postNew);
 
 module.exports = route;
