3ce3ec7590c24b3bf316e35d3dc71b22da63d111
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-04 14:56:07 +0000

    edit port

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-
 app.use('/', routes);
 
-app.listen(port => {
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
